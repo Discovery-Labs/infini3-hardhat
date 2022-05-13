98caba2b664b97d2391c1194c1a1bf18a88c1d05
Jamie Pickett <jpickett713@gmail.com>
2022-05-13 17:51:10 -0400

    added mint function tied into dCompToken address

--- contracts/AdventurerSFT.sol
+++ contracts/AdventurerSFT.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /**
  * @title AdventurerSFT
@@ -10,6 +12,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 
 contract AdventurerSFT is ERC1155, Ownable {
+    using SafeERC20 for IERC20;
+
     uint256 public tokenIdCounter;
     address projectNFTAddr;
     address dCompTokenAddr;
@@ -26,6 +30,8 @@ contract AdventurerSFT is ERC1155, Ownable {
     }
 
     mapping(string => mapping (uint => uint)) public optionTokenIdByProjectIdAndVersion;
+
+    mapping(uint => uint) public totalSupplyPerTokenId;
 
     mapping(uint256 => AdventurerTokenInfo) public adventurerTokenInfoByTokenId;
 
@@ -53,5 +59,24 @@ contract AdventurerSFT is ERC1155, Ownable {
         adventurerTokenInfo.version = _version;
         adventurerTokenInfo.priceInDComp = _price;
         adventurerTokenInfo.maximumCap = _maxCap;
+    }
+
+    function mint(
+        address _to,
+        string memory _projectId,
+        uint256 _version
+    ) external{
+        require(msg.sender == projectNFTAddr, "ADVENTURER_SFT: ONLY_PROJECT");
+        uint tokenId = optionTokenIdByProjectIdAndVersion[_projectId][version];
+        require(tokenId > 0, "ADVENTURER_SFT: INVALID_TOKEN_ID");
+        require(balanceOf(_to, tokenId) == 0, "ADVENTURER_SFT: DUPLICATE_MINT");
+        uint currentSupply = totalSupplyPerTokenId[tokenId];
+        AdventurerTokenInfo memory adventureTokenInfo = adventurerTokenInfoByTokenId[tokenId]; 
+        require(adventureTokenInfo.maximumCap == 0 || currentSupply < adventureTokenInfo.maximumCap, "ADVENTURER_SFT: CAP_REACHED");
+        totalSupplyPerTokenId[tokenId]++;
+        if(adventureTokenInfo.priceInDComp > 0){
+            IERC20(dCompTokenAddr).safeTransferFrom(_to, address(this), adventureTokenInfo.priceInDComp);
+        }
+        _mint(_to, tokenId, 1, "");
     }
 }
